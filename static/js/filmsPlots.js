0b4591adb23d306cf2361c006f5333f40204c92d
Mubashira Qari <81127634+uzmabb182@users.noreply.github.com>
2021-12-12 15:13:54 -0500

    Working on JavaScript commands application

---
+++ static/js/filmsPlots.js
@@ -0,0 +1,97 @@
+console.log(films);
+
+// Initialized arrays
+let gRating = []
+let pgRating = []
+let pg13Rating = []
+let rRating = []
+
+for (let i = 0; i < films.length; i++) {
+    row = films[i];
+    console.log(row)
+    gRating.push(row.rating);
+}
+
+console.log(gRating);
+console.log(gRating[0]);
+
+let rating = ['G', 'PG', 'PG-13', 'R'];
+
+let newArray = [];
+
+// 1st way:
+for (let i = 0; i < rating.length; i++) {
+    newArray.push(`more ${rating[i]}`)
+};
+
+console.log(rating);
+console.log(newArray);
+
+// 2nd way:
+rating.forEach(function(item) {
+    newArray.push(`even more ${item}`);
+});
+
+console.log(newArray);
+
+// 2nd way
+
+let rate = rating.forEach(item => console.log(item))
+
+// 3rd way:
+let mapArray = rating.map(function(item) {
+    return `even even more ${item}`
+});
+
+console.log(mapArray);
+
+// 4th way:
+let indexmapArray = rating.map(function(item, index) {
+    return `even even more ${item}-${index}`;
+});
+
+console.log(indexmapArray);
+
+// Now examples with array of objects
+
+let students = [
+    {name:"Malcom", score: 80},
+    {name:"Zoe", score: 85},
+    {name:"Kaylee", score: 99},
+    {name:"Simon", score: 95},
+    {name:"Wash", score: 79}
+]
+
+let names = students.map(function(item, index){
+    return `${item.name}-${index}`;
+});
+
+console.log(names);
+
+// 5th way
+let scoreList = students.map((item, index) => `${item.score}-${index}`)
+console.log(scoreList);
+
+// ---------------------------------------------------
+// filtering array
+let scoreFilter = students.filter(item => item.score >= 80)
+console.log(scoreFilter);
+// Now to get the individual item in the filtered list
+let nameList = scoreFilter.map((item, index) => `${item.score}-${index}`)
+console.log(nameList);
+console.log(nameList.length);
+
+//-----------------------------------------------------
+// Slicing of array
+let firstName = ["Jane", "John", "Jimbo", "Jedah"];
+// Slice the first two row
+let left = firstName.slice(0, 2);
+console.log(left);
+
+let right = firstName.slice(2, 4);
+console.log(right);
+
+// Sorting the array descending is (b-a)
+
+
+
